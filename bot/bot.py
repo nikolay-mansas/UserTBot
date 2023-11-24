8bd4642cf3f8f87396e4ec5c1d444a9fc594a950
nikolay-mansas <dokykin0nikolay@gmail.com>
2023-11-24 23:21:13 +0500

    Add api_id, api_hash. Start tested bot.

--- bot/bot.py
+++ bot/bot.py
@@ -1,8 +1,10 @@
 from telethon.sync import TelegramClient, events
+import logging as log
 
 accept_list_users: list[int] = []
 
-async def start_bot(api_id: int, api_hash: str, version: str):
+
+async def start_bot(api_id: int, api_hash: str, version: str, logging: log):
     with TelegramClient("session", api_id, api_hash) as client:
         client.send_message("me", f"Bot started\nversion: {version}")
 
--- main.py
+++ main.py
@@ -1,19 +1,33 @@
+from bot import bot
 import configparser
-import telebot
 import logging
+import asyncio
 import time
 import sys
 import os
 
 config = configparser.ConfigParser()
 config.read("bot/config.ini")
+
+api_hash = config["BOT"]["api_hash"]
+if api_hash == "" or api_hash == " ":
+    raise Exception('Api_hash in file "bot/config.ini" is null')
+
+try:
+    api_id = config["BOT"]["api_id"]
+    if api_id == "" or api_id == " ":
+        raise Exception('Api_id in file "bot/config.ini" is null')
+    api_id = int(api_id)
+except TypeError:
+    raise Exception('Api_id in file "bot/config.ini" is type error')
+
 log_dir = config["SCRIPT"]["log_dir"]
 if log_dir == "" or log_dir == " ":
     raise Exception('Log_dir in file "bot/config.ini" is null')
+
 log_level = config["SCRIPT"]["log_level"]
 if log_level == "" or log_level == " ":
     raise Exception('Log_level in file "bot/config.ini" is null')
-print(log_level.lower())
 match log_level.lower():
     case "info":
         log_level = logging.INFO
@@ -21,6 +35,10 @@ match log_level.lower():
         log_level = logging.DEBUG
     case _:
         raise Exception(f"Log level not defined, you input log level: {log_level.lower()}")
+
+version = config["SCRIPT"]["version"]
+if version == "" or version == " ":
+    raise Exception('Version in file "bot/config.ini" is null')
 
 try:
     os.mkdir(log_dir)
@@ -35,4 +53,7 @@ logging.basicConfig(
         logging.StreamHandler(sys.stdout)
     ]
 )
+
 logging.info("Initialization and code loading is successful, telegram bot loading.")
+
+asyncio.run(bot.start_bot(api_id=api_id, api_hash=api_hash, version=version, logging=logging))
